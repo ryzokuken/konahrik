47b2988699148e6c2397c43e889c60a80940b6ef
Ujjwal Sharma <usharma1998@gmail.com>
2018-07-03 16:00:32 +0530

    Trace Krosis

--- krosis/app.py
+++ krosis/app.py
@@ -3,9 +3,21 @@ import time
 import redis
 from flask import Flask
 
+from jaeger_client import Config
+from flask_opentracing import FlaskTracer
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
+def initialize_tracer():
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+            'local_agent': {'reporting_host': 'jaeger'}
+        },
+        service_name='krosis'
+    )
+    return config.initialize_tracer()
 
 
 def get_hit_count():
@@ -21,9 +33,10 @@ def get_hit_count():
 
 
 @app.route('/')
-def hello():
+def index_krosis():
     count = get_hit_count()
     return 'Krosis {}\n'.format(count)
 
 if __name__ == "__main__":
+    flask_tracer = FlaskTracer(initialize_tracer, True, app)
     app.run(host="0.0.0.0", debug=True)
