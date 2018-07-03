b5ec4b58164a80e8e30e2b00a109c291ccc956f6
Ujjwal Sharma <usharma1998@gmail.com>
2018-07-03 16:00:21 +0530

    Trace Vokun

--- vokun/app.py
+++ vokun/app.py
@@ -3,15 +3,27 @@ import time
 from flask import Flask
 import requests
 
+from jaeger_client import Config
+from flask_opentracing import FlaskTracer
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
+def index_vokun():
     krosis = requests.get('http://krosis:5000').text
     rahgot = requests.get('http://rahgot:5000').text
     return "Krosis: {}\nRahgot: {}\n".format(krosis, rahgot)
 
+def initialize_tracer():
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+            'local_agent': {'reporting_host': 'jaeger'}
+        },
+        service_name='vokun'
+    )
+    return config.initialize_tracer()
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    flask_tracer = FlaskTracer(initialize_tracer, True, app)
+    app.run(host='0.0.0.0')
