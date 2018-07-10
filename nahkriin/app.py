4e5d1674b1ff78ace0efc72d5257645c79b47789
Ujjwal Sharma <usharma1998@gmail.com>
2018-07-10 11:38:02 +0530

    Trace EVERYTHING

--- nahkriin/app.py
+++ nahkriin/app.py
@@ -1,14 +1,16 @@
-import time
-
 from flask import Flask
-
+from grtracer import GrTracer
 
 app = Flask(__name__)
+tracer = GrTracer(app, 'nahkriin')
 
 
 @app.route('/')
 def hello():
     return "Nahkriin\n"
+
+
+tracer.bootstrap()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- rahgot/app.py
+++ rahgot/app.py
@@ -1,16 +1,18 @@
-import time
-
 from flask import Flask
 import requests
-
+from grtracer import GrTracer
 
 app = Flask(__name__)
+tracer = GrTracer(app, 'rahgot')
 
 
 @app.route('/')
 def hello():
     nahkriin = requests.get('http://nahkriin:5000').text
     return "Rahgot\n Nahkriin: {}".format(nahkriin)
+
+
+tracer.bootstrap()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
